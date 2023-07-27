210ed0e69e1597cb77215c8e8ba2adc704376560
JessicaBane <jessicambane@gmail.com>
2023-07-27 17:54:09 +0100

    working server

---
+++ library/weather.js
@@ -0,0 +1,35 @@
+const data = require("../data/weather.json")
+
+function weather (request, response){
+    try{
+        let dataToReturn = "";
+
+        function filterCities(lat, lon){
+            console.log(lat)
+            console.log(lon)
+            const searchedCity = data.find((city)=>city.lat == lat && city.lon == lon)
+            console.log(searchedCity)
+            if(!searchedCity){
+                response.json("Sorry no matching city found for " + request.query.value)
+            } else{
+                response.json(searchedCity.data)
+            }
+        }
+
+        if(request.query.lat && request.query.lon){
+            dataToReturn = filterCities(request.query.lat, request.query.lon)
+        }
+        if(dataToReturn){
+            response.status(200).json(dataToReturn)
+        } 
+        //else {
+        //     console.log("Something is wrong with those co-ordinates")
+        //     response.json("no data to be displayed")
+        //     // response.status(400).json("no data to be displayed")
+        // }
+    }catch(error){
+        console.log(error)
+    }
+}
+
+module.exports=weather
--- server.js
+++ server.js
@@ -1,41 +1,21 @@
 const express = require("express")
 const cors = require("cors")
 require("dotenv").config()
-const data = require("./data/weather.json")
 const axios = require("axios")
 const app = express()
 app.use(cors())
 
 
 const PORT = process.env.PORT || 8082
+const weather = require("./library/weather")
 
 console.log("server running")
 
-
-function filterCities(lat, lon){
-    console.log("lat ", lat)
-    console.log("lon ", lon)
-}
-
 app.get("/", (request, response) => {
-    response.status(200).json("Server is running on / " + PORT)
+    response.status(200).json("Server is running on / " + PORT + " This is root")
 });
 
-app.get("/weather", (request, response)=>{
-    let dataToReturn = "";
-    console.log(request + "HERE")
-    if(request.query.lat && request.query.lon){
-        dataToReturn = filterCities(request.query.lat, request.query.lon)
-    }
-    if(dataToReturn){
-        response.status(200).json(dataToReturn)
-    } else {
-        console.log("Something is wrong with those co-ordinates")
-        response.json("no data to be displayed")
-        // response.status(400).json("no data to be displayed")
-    }
-    
-})
+app.get("/weather", weather)
 
 app.listen(PORT, ()=> console.log(`app is running on port ${PORT}`))
 
